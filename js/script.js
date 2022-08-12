f485c0450b23c5ffded5db71476c41c14f26b01d
Дмитрий <105663726+YgamiJS@users.noreply.github.com>
2022-08-13 00:20:32 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,13 +5,18 @@ function func(){
     if(document.getElementById("Lines")){
         document.getElementById("Lines").remove();
     }
+    let header = document.querySelector("header");
     var rows = document.querySelector('textarea').value.split("\n").length;
     div_insert.insertAdjacentHTML("afterbegin" , `<div id="Lines">Строк: ${rows}</div>`);
     let value = document.querySelector("textarea").value;
 
     try{
         eval(value);
+        header.style.backgroundColor = "#238636";
+        setTimeout( () => header.style.backgroundColor = "" , 1000);
     }catch(error){
-        alert(error);
+        setTimeout( () => alert(error) , 1000);
+        header.style.backgroundColor = "#b81710";
+        setTimeout( () => header.style.backgroundColor = "" , 1000);
     }
 }
